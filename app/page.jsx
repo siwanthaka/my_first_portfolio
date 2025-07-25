9d150252c9f3f1e59fba31c5d06d1103c342088e
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-07-25 12:47:33 +0530

    Design more of homepage

--- app/page.jsx
+++ app/page.jsx
@@ -2,21 +2,24 @@ import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
 
 
  
 
 const Home = () => {
-  return <section className="h-full">
+  return (
+  <section className="h-full">
     <div className="container mx-auto h-full">
-      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-1 xl:pb-8">
         {/*text*/}
-        <div className="text-center xl:text-left order-2 xl:order-none">
+        <div className="text-center xl:text-left order-2 xl:order-none -mt-10">
           <span className="text-xl">Software Engineer</span>
-          <h1 className="h1 mb-6">
-            Hello I'm <br /> <span className="text-accent">Siwanthaka Savinda</span>
+          <h1 className="h1 mb-5">
+            Hello I'm <br /> <span className="text-accent">Siwanthaka</span>
           </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">
+          <p className="max-w-[500px] mb-6 text-white/80">
             I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.
           </p>
           {/* btn and socials*/}
@@ -40,7 +43,9 @@ const Home = () => {
         </div>
       </div>
     </div>
-  </section>;
+    <Stats />
+  </section>
+  );
 };
 
 export default Home;
