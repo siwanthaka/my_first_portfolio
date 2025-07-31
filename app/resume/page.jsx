602547d25fec11268b81cbf6b51307416c47e4f1
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-07-31 23:42:33 +0530

    complete skill page of resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -99,10 +99,36 @@ const education = {
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eius temporibus adffdt tu piyf gutr",
     items: [
         {
-            Campus: "Dev Wizards pvt ltd",
-            Degree: "(BSc) Hons in Software Engineering",
-            duration: "2023 - 2027",
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
         },
+        {
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
+        },
+        {
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
+        },
+        {
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
+        },
+        {
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
+        },
+        {
+            institution: "Dev Wizards pvt ltd",
+            degree: "Back End developer",
+            duration: "2025 - present",
+        },
+        
     ],
 };
 
@@ -177,8 +203,9 @@ const Resume = () => {
                     <TabsTrigger value="about">About me</TabsTrigger>
                 </TabsList>
 
-                {/* content */}
-                <div className="min-h-[70vh] w-full">
+                    {/* content */}
+                    <div className="min-h-[70vh] w-full">
+
                     {/* experience */}
                     <TabsContent value="experience" className="w-full">
                         <div className="flex flex-col gap-[30px] text-center xl:text-left">
@@ -195,7 +222,7 @@ const Resume = () => {
                                             <div className="flex items-center gap-3">
                                                 {/*dot*/}
                                                 <span className="w-[60px] h-[6px] rounded-full bg-accent"></span>
-                                                <p className="text-white/60">{item.company}</p>
+                                                <p className="text-white">{item.company}</p>
                                             </div>
                                         </li>
                                     ))}
@@ -203,13 +230,58 @@ const Resume = () => {
                             </ScrollArea>
                         </div>
                     </TabsContent>
+
                     {/* education */}
                     <TabsContent value="education" className="w-full">
-                        education
+                         <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                            <h3 className="text-4xl font-bold">{education.title}</h3>
+                            <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                                {education.description}
+                            </p>
+                            <ScrollArea className="h-[400px]">
+                                <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                                    {education.items.map((item, index) => (
+                                        <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+                                            <span className="text-accent">{item.duration}</span>
+                                            <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
+                                            <div className="flex items-center gap-3">
+                                                {/*dot*/}
+                                                <span className="w-[60px] h-[6px] rounded-full bg-accent"></span>
+                                                <p className="text-white">{item.institution}</p>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </ScrollArea>
+                        </div>
                     </TabsContent>
+
                     {/* skills */}
-                    <TabsContent value="skills" className="w-full">
-                        skills 
+                    <TabsContent value="skills" className="w-full h-full">
+                        <div className="flex flex-col gap-[30px]">
+                            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                <h3 className="text-4xl font-bold">{skills.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                                {skills.description}
+                                </p>
+                            </div>
+                            <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                                {skills.skillList.map((skill, index) => {
+                                    return <li key={index}>
+                                        <TooltipProvider delayDuration={100}>
+                                            <Tooltip>
+                                                <TooltipTrigger className="w-full h-[150px] bg-[#5f625f33] rounded-xl flex justify-center items-center group">
+                                                    <div className="text-6xl group-hover:text-accent transition-all duration-300">{skill.icon}</div>
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p className="capitalize">{skill.name}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </li>;
+                                })}
+                            </ul>
+                        </div>
                     </TabsContent>
                     {/* about me */}
                     <TabsContent value="about" className="w-full">
