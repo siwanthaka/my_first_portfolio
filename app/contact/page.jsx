8a05d1528c67a5f47dd15121d5ff1be9f3a12b85
Savinda De Silva <siwanthakasavinda@gmail.com>
2025-08-05 23:17:48 +0530

    start working on contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,90 @@
+"use client";
+
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
+
+import { 
+    Select, 
+    SelectContent, 
+    SelectGroup, 
+    SelectItem, 
+    SelectLabel, 
+    SelectTrigger, 
+    SelectValue,
+ } from "@radix-ui/react-select";
+
+ import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+ const info = [
+    {
+        icon: <FaPhoneAlt />,
+        title: "Phone",
+        description: "(+94) 75 081 8024",
+    },
+    {
+        icon: <FaEnvelope />,
+        title: "Email",
+        description: "siwanthakasavinda@gmail.com",
+    },
+    {
+        icon: <FaMapMarkerAlt />,
+        title: "Address",
+        description: "CINEC campus Malabe",
+    },
+ ];
+
+ import { motion } from "framer-motion";
+
 const Contact = () => {
-    return <div>contact page</div>;
+    return (
+    <motion.section 
+        initial={{ opacity: 0 }}
+        animate={{ 
+            opacity: 1,
+            transition: {delay: 2.4, duration: 0.4, ease:"easeIn"},
+          }}
+
+          className="py-6"
+          >
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-[30px] ">
+                    {/* form */}
+                    <div className="xl:h-[54%] order-2 xl:order-none">
+                        <form  className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
+                            <h3 className="text-4xl text-accent">Let's work together</h3>
+                            <p className="text-white/60">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quidem nostrum debitis nt nam.
+                            </p>
+                            {/* input */}
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input type="firstname" placeholder="Firstname"/>
+                                <Input type="lastname" placeholder="Lastname"/>
+                                <Input type="email" placeholder="Email address"/>
+                                <Input type="phone" placeholder="Phone number"/>
+                            </div>
+                            {/* select */}
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a service" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                  <SelectGroup>
+                                    <SelectLabel>Select a service</SelectLabel>
+                                    <SelectItem value="est">Web Development</SelectItem>
+                                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                                    <SelectItem value="mst">Logo Design</SelectItem>
+                                </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </form>
+                    </div>
+                    {/* info */}
+                    <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+                </div>
+            </div>
+    </motion.section>
+        );
 };
 
 export default Contact;
--- components/ui/select.jsx
+++ components/ui/select.jsx
@@ -16,7 +16,7 @@ const SelectTrigger = React.forwardRef(({ className, children, ...props }, ref) 
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex h-[48px] w-full items-center justify-between rounded-md border border-white/10 bg-primary px-4 py-5 text-base text-white/60 placeholder:text-white/10 focus:border-accent outline-none",
       className
     )}
     {...props}>
